6d26d6614a782c8646784dfd2e715347b7b71fa9
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2023-09-06 10:34:31 +0200

    asd

--- elso/elso/Program.cs
+++ elso/elso/Program.cs
@@ -11,6 +11,12 @@ namespace elso
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Word!");
+            int szam = 6; Console.WriteLine(szam); 
+            int nszam = -15; Console.WriteLine(nszam);
+            double tszam = 12.5; Console.WriteLine(tszam);
+            char karakter = '%'; Console.WriteLine(karakter);
+            Console.WriteLine(szam + nszam);
+
         }
     }
 }

3f7f84dd58f2a4e8b322d7ea9a382a18a5688df7
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2023-11-17 11:30:07 +0100

    asd

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -1,3 +1,21 @@
-﻿Dictionary<int,string> lapok = new Dictionary<int,string>();
-Random random = new Random();
-Console.WriteLine(random.Next(52));
+﻿/*
+ * Random szám ami a a kártya kulcsát adja meg majd ha a kártya értékének kiírása egy másik tömbe hogy ne tudja mégeygszer kihúzni
+ * Kártya hozzá rendelése a játékos hoz és a kártya értékének megadása*/
+string kezdes = "";
+while (true)
+{
+    Console.Write("Kezdőthet a játék? (i/n) : ");
+    kezdes = Console.ReadLine();
+    if (kezdes == "i")
+    {
+        break;
+    }
+    else if (kezdes == "n")
+    {
+        break;
+    }
+    else {
+        Console.WriteLine("Kis i vagy n betű!");
+    }
+}
+

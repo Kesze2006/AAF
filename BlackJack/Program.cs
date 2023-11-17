dece4a0393b7ad284d663aae530dff7103767734
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2023-11-17 13:18:44 +0100

    asd

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -1,6 +1,8 @@
 ﻿/*
  * Random szám ami a a kártya kulcsát adja meg majd ha a kártya értékének kiírása egy másik tömbe hogy ne tudja mégeygszer kihúzni
  * Kártya hozzá rendelése a játékos hoz és a kártya értékének megadása*/
+//Panel
+/*
 string kezdes = "";
 while (true)
 {
@@ -15,7 +17,47 @@ while (true)
         break;
     }
     else {
-        Console.WriteLine("Kis i vagy n betű!");
+        Console.WriteLine("Kis -i- vagy -n- az engedélyezett!");
+    }
+}
+if (kezdes == "i")
+{
+    //jatek();
+}
+else
+{
+    Console.WriteLine("Viszlát!");
+}
+*/
+//Panel vége
+//Felület
+Console.WriteLine();
+Console.Write(" Osztó: kártya kártya");
+Console.WriteLine();
+Console.Write(" Játékos: kártya kártya");
+Console.WriteLine();
+bool kell = true;
+while (kell)
+{
+    Console.Write("Kér lapot? i/n : ");
+    string lap = Console.ReadLine();
+    switch (lap)
+    {
+        case "i":
+            {
+                kell = false;
+                break;
+            }
+        case "n":
+            {
+                kell= false;
+                break;
+            }
+        default:
+            {
+                Console.WriteLine("Kis -i- vagy -n- az engedélyezett!");
+                break;
+            }
     }
 }
 

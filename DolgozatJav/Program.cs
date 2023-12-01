e71a1f6212d25c6919249ba7ecde075febd2b31d
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2023-12-01 13:18:58 +0100

    asd

--- DolgozatJav/Program.cs
+++ DolgozatJav/Program.cs
@@ -40,7 +40,7 @@ void betolt2()
 }
 void betolt3()
 {
-    string[] sorok = File.ReadAllLines("adathalmaz.csv");
+    string[] sorok = File.ReadAllLines("adathalmaz.csv").ToList().Skip(1).ToArray();
 
     foreach(string sor in sorok)
     {
@@ -52,4 +52,31 @@ void betolt3()
         ho4.Add(Convert.ToDouble(adat[4]));
         darab.Add(Convert.ToInt32(adat[5]));
     }
+}
+
+betolt3();
+Console.WriteLine("2. feladat\n Ennyi adatsor van {0}.",darab.Count);
+
+Console.WriteLine("3. feladat\n Átlagos napi termelés {0} db/óra.", darab[darab.Count - 1] / 24);
+
+Console.WriteLine("4. feladat\n Min1 {0}\n Min2 {1}\n Min3 {2}\n Min4 {3}",ho1.Min(),ho2.Min(),ho3.Min(),ho4.Min());
+
+double maximum = 0;
+int maxIndex = 0;
+for (int i = 0; i < ho1.Count; i++)
+{
+    if (ho1[i] > maximum)
+    {
+        maximum = ho1[i];
+        maxIndex= i;
+    }
+}
+
+Console.WriteLine("5. feladat\n {0} fok, {1} perc", maximum, percek[maxIndex]);
+
+List<int> darab5perc = new List<int>();
+darab5perc.Add(darab[0]);
+for (int i = 0; i < darab.Count - 1; i++)
+{
+    
 }

4b5cf341d3ec1ade6f6f8dbef7acfe5fc13a7cdf
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2024-01-30 10:28:53 +0100

    asd

---
+++ FogadoOra/fogado/Program.cs
@@ -0,0 +1,77 @@
+﻿using fogado;
+
+List<adatok> foglalasok = new List<adatok>();
+
+StreamReader olvas = new StreamReader("fogado.txt");
+
+while (!olvas.EndOfStream)
+{
+    
+    string sor = olvas.ReadLine();
+    foglalasok.Add(new adatok(sor));    
+
+}
+olvas.Close();
+
+//2.feladat
+Console.WriteLine("2.feladat:");
+Console.WriteLine("{0} darab foglalás volt",foglalasok.Count());
+
+Console.WriteLine();
+//3.feldat
+Console.WriteLine("3.feladat:");
+Console.Write("Kérem az egyik tanár nevét: ");
+string nev = Console.ReadLine();
+
+int darab = 0;
+for (int i = 0; i < foglalasok.Count(); i++)
+{
+    if (nev == foglalasok[i].nev)
+    {
+        darab++;
+    }
+}
+if (darab == 0)
+{
+    Console.WriteLine("Az adott néven nincs időpont foglalás: {0}", nev);
+}
+else
+{
+    Console.WriteLine("{0} darab foglalás volt {1} néven",darab, nev);
+}
+Console.WriteLine();
+
+//4.feladat
+Console.WriteLine("4.feladat:");
+Console.WriteLine("Adjon meg egy érvényes időpontot(pl. 17:40): ");
+string idopont = Console.ReadLine();
+List<string> nevek = new List<string>();
+
+for (int i = 0; i < foglalasok.Count(); i++)
+{
+    if (idopont == foglalasok[i].idopont)
+    {
+        nevek.Add(foglalasok[i].nev);
+    }
+}
+
+nevek.Sort();
+
+for (int i = 0; i < nevek.Count(); i++)
+{
+    Console.WriteLine(nevek[i]);
+}
+
+File.WriteAllLines(idopont.Replace(":", "")+".txt",nevek);
+Console.WriteLine();
+//5.feldat
+adatok min = foglalasok[0];
+for (int i = 0; i < foglalasok.Count(); i++)
+{
+    if (foglalasok[i].foglalas < min.foglalas)
+    {
+        min = foglalasok[i];
+    }
+}
+Console.WriteLine("5.feldat:");
+Console.WriteLine(min);
---
+++ FogadoOra/fogado/adatok.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace fogado
+{
+    internal class adatok
+    {
+        public string nev;
+        public string idopont;
+        public DateTime foglalas;
+
+        public adatok(string sor)
+        {
+
+            string[] adatok = sor.Split(" ");
+            nev = adatok[0]+" "+adatok[1];
+            idopont = adatok[2];
+            foglalas = Convert.ToDateTime(adatok[3].Replace("-", " "));
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Tanár neve: {0}\r\nFoglalt időpont: {1}\r\nFoglalás ideje: {2}",nev,idopont,foglalas);
+        }
+
+    }
+}

d08da43e272527659a0a53765b5b9fe94795bac5
Ákos Keszericze <keszericze.akos.21@ady-nagyatad.hu>
2024-04-12 11:30:15 +0200

    asd

--- tort/MainWindow.xaml.cs
+++ tort/MainWindow.xaml.cs
@@ -24,5 +24,94 @@ namespace tort
         {
             InitializeComponent();
         }
+
+
+        private int lnko(int a, int b)
+        {
+            if (a == b)
+            {
+                return a;
+            }
+            else if (a < b)
+            {
+                return lnko(a, b - a);
+            }
+            else if (a > b)
+            {
+                return lnko(a - b, b);
+            }
+            return 1;
+        }
+
+        private void szbekero_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int szamlalo = 0;
+            try
+            {
+                szamlalo = Convert.ToInt32(szbekero.Text);
+            }
+            catch
+            {
+            }
+            int nevezo = 0;
+            try
+            {
+                nevezo = Convert.ToInt32(nbekero.Text);
+            }
+            catch
+            {
+            }
+
+
+            if (nevezo != 0 && szamlalo % nevezo == 0)
+            {
+                kiir.Text = "" + szamlalo + "/" + nevezo;
+            }
+            else if (nevezo != 0 && szamlalo % nevezo != 0)
+            {
+                kiir.Text = "Nem egész!";
+            }
+            else
+            {
+                kiir.Text = "Nullával való osztás";
+            }
+            textblock1.Text = szamlalo.ToString();
+            textblock2.Text = nevezo.ToString();
+
+            if (nevezo != 0 && szamlalo != 0)
+            {
+                int oszto = lnko(szamlalo, nevezo);
+
+                textblock3.Text = (szamlalo / oszto).ToString();
+                textblock4.Text = (nevezo / oszto).ToString();
+                _4_fel.IsEnabled = true;
+            }
+            else
+            {
+                _4_fel.IsEnabled = false;
+            }
+        }
+
+        private void szbekero1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int szamlalo = 0;
+            try
+            {
+                szamlalo = Convert.ToInt32(szbekero1.Text);
+            }
+            catch
+            {
+            }
+            int nevezo = 0;
+            try
+            {
+                nevezo = Convert.ToInt32(nbekero1.Text);
+            }
+            catch
+            {
+            }
+            textblock5.Text = szamlalo.ToString();
+            textblock6.Text = nevezo.ToString();
+        }
     }
 }

1941fa15c93caff5df7d3a895819dc70a851d8d4
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2024-09-03 11:37:24 +0200

    asd

---
+++ 2023-2024/HFHanySzamjegyu/HFHanySzamjegyu/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFHanySzamjegyu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int szam = -1;
+
+            while(szam < 0 && szam < 999999 || szam < 0 && szam > -999999)
+            try
+            {
+                Console.Write("Kérek egy számot 0 és 999.999 között: ");
+                szam = Convert.ToInt32(Console.ReadLine());
+                if (szam < 0)
+                {
+                    break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Nem számot adott meg!");
+                Console.WriteLine();
+            }
+            int szamjegy = 0;
+            if (szam == 0)
+            {
+                szamjegy += 1;
+            }
+            while (szam > 0)
+            {
+                szam /= 10;
+                szamjegy += 1;
+            }
+            Console.WriteLine("A szám "+szamjegy+" számjegyből áll");
+        }
+    }
+}
---
+++ 2023-2024/StringHF/StringHF/Program.cs
@@ -0,0 +1,38 @@
+﻿string szoveg = "";
+bool vizsga = false;
+while (vizsga != true)
+{
+    Console.Write("Kérek egy több szóból álló szöveget amely szóközzel van elválasztva: ");
+    szoveg = Console.ReadLine();
+    string [] kell = new string[szoveg.Length];
+    kell = szoveg.Split(" ");
+    string[] tomb = new string[kell.Length];;
+   
+    if (kell.Length <= 1)
+    {
+        Console.WriteLine("Rövid vagy nem szóközzel van elválasztva!");
+        Console.WriteLine();
+        continue;
+    }
+    else {
+        for (int i = 0; i < kell.Length; i++)
+        {
+            string valtozo = "";
+            string elso = "";
+            string jo = "";
+            valtozo = kell[i];
+            elso = valtozo[0].ToString().ToUpper();
+            jo = valtozo.Remove(0, 1);
+            valtozo = elso + jo;
+            tomb[i] = valtozo;
+        }
+        for (int i = 0; i < tomb.Length; i++)
+        {
+            Console.Write(tomb[i]+" ");
+        }
+        Console.WriteLine();
+        vizsga = true;
+    }
+}
+
+

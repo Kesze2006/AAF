7ca0c611be4ae7550529682c8dae62dac634e2a5
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2024-09-11 12:32:31 +0200

    asd

---
+++ 2024-2025/gyak0911/Program.cs
@@ -0,0 +1,30 @@
+﻿StreamReader olvas = new StreamReader("fajl.txt");
+StreamWriter ir = new StreamWriter("jofajl.txt");
+while (!olvas.EndOfStream)
+{
+    string line = olvas.ReadLine();
+    if (line[0] == ' ')
+    {
+        ir.WriteLine(line.Remove(0,1));
+    }
+}
+ir.Close();
+olvas.Close();
+
+static List<string> beolvas(string fajl)
+{
+    List<string> mondatok = new List<string>();
+    StreamReader olvas = new StreamReader(fajl);
+    while (!olvas.EndOfStream)
+    {
+        mondatok.Add(olvas.ReadLine());
+    }
+    olvas.Close();
+    return mondatok;
+}
+
+
+
+
+
+

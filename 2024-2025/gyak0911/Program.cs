c8dae6a63017c444e6c109e4f2da2e57ab9d3180
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2024-09-11 22:05:47 +0200

    as

--- 2024-2025/gyak0911/Program.cs
+++ 2024-2025/gyak0911/Program.cs
@@ -23,7 +23,26 @@ static List<string> beolvas(string fajl)
     return mondatok;
 }
 
+static List<string> feladat(List<string> mondatok)
+{
+    List<string> joszavak = new List<string>();
+    Random random = new Random();
+    for (int i = 0; i < mondatok.Count; i++)
+    {
+        List<string> szavak = new List<string>();
+        string sor = mondatok[i];
+        szavak = sor.Split(" ").ToList();
+        joszavak.Add(szavak[random.Next(0,szavak.Count-1)]);
+    }
 
+    return joszavak;
+}
+
+List<string> strings = feladat(beolvas("jofajl.txt"));
+for (int i = 0; strings.Count > i; i++)
+{
+    Console.WriteLine(strings[i]);
+}
 
 
 

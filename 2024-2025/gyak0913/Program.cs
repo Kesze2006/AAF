b65d689d25be87599314f8b91648d46ef56732bf
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2024-09-13 14:18:10 +0200

    asd

---
+++ 2024-2025/gyak0913/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak0913
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kutya k = new kutya("fasz");
+
+            k.ugat();
+            k.suly = -145;
+            Console.WriteLine(k.suly);
+            Console.WriteLine(kutya.ezMiEz());
+        }
+    }
+}
---
+++ 2024-2025/gyak0913/kutya.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak0913
+{
+    internal class kutya
+    {
+        public string nev;
+
+        public void ugat()
+        {
+            Console.WriteLine("wauawuauwauwuauwuauwauwua");
+        }
+
+        private float _suly;
+        public float suly
+        {
+            
+            get
+            {
+                return _suly;
+            }
+            set
+            {
+                _suly = Math.Abs(value);
+            }
+        }
+
+        public kutya(string nev)
+        {
+            Console.WriteLine("kész a kutya");
+            this.nev = nev;
+
+        }
+
+        public override string ToString()
+        {
+            return "Kutya vagy!";
+        }
+
+        public static string ezMiEz()
+        {
+            return "Ez egy kutya ";
+        }
+    }
+}

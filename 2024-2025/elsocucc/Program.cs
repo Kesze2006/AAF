120634b0e68cb424d639cd12128c825c9b9b4f15
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2024-09-16 14:11:16 +0200

    asd

---
+++ 2024-2025/elsocucc/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elsocucc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter ir = new StreamWriter("fajl.txt");
+            while (true)
+            {
+                Console.WriteLine("Kérek egy számot:");
+                int szam = Convert.ToInt32(Console.ReadLine());
+                ir.WriteLine(szam+" "+szam*szam);
+                if (szam == 0)
+                {
+                    break;
+                    
+                }  
+            }
+            ir.Close();
+
+            List<int> szamlista  = new List<int>();
+            List<int> negyzetlista = new List<int>();
+            StreamReader olvas = new StreamReader("fajl.txt");
+            while (olvas.EndOfStream != true)
+            {
+                Console.WriteLine(olvas.ReadLine());
+
+            }
+            Console.WriteLine("Összesen {0} sorm van benne");
+        }
+    }
+}

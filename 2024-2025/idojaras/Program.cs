875a4431451a161193b9823faf0273bec3949052
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2024-09-20 09:37:20 +0200

    asd

---
+++ 2024-2025/idojaras/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq.Expressions;
+
+Random szam =  new Random();
+int fok = szam.Next(10, 40);
+for (int k = 0; k < 7; k++)
+{
+    
+    for (int i = 0; i < 24; i++)
+    {
+        int csok = szam.Next(0, 5);
+        if (i <= 8 || i >= 20)
+        {
+            fok = fok - csok;
+            if (fok < 10)
+            {
+                fok += csok;
+            }
+
+        }
+        else
+        {
+            fok = fok + csok;
+            if (fok >= 40)
+            {
+                fok -= csok;
+            }
+        }
+        Console.WriteLine(fok);
+        
+    }
+    Console.WriteLine("Nap vége");
+}

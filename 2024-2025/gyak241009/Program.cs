3dbc3f4e7d6a06bd9b8afc691c49cd12512886c8
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2024-10-09 13:28:52 +0200

    asd

---
+++ 2024-2025/gyak241009/Program.cs
@@ -0,0 +1,6 @@
+﻿using gyak241009;
+
+Hajo hajo = new Hajo();
+Vitorlas vitorlas = new Vitorlas();
+hajo.megfordul();
+Console.WriteLine(vitorlas.nev);
---
+++ 2024-2025/gyak241009/hajo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak241009
+{
+    interface Ihajo
+    {
+        void megfordul();
+        void mozgas();
+        void duda();
+        void sullyed();
+    }
+    class Hajo : Ihajo
+    {
+        public void megfordul()
+        {
+            Console.WriteLine("A hajó forog");
+        }
+        public void mozgas()
+        {
+            
+        }
+        public void duda()
+        {
+            
+        }
+        public void sullyed()
+        {
+            
+        }
+        public int hossz = 15;
+        public string nev = "SSBix";
+        public int vizkiszoritas = 50;
+    }
+
+    class Vitorlas : Hajo
+    {
+        public string nev = "vitorlas";
+    }
+
+    class Csonak : Hajo
+    {
+        
+    }
+}

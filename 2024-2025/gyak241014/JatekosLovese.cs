2ed918589216391a67e50ee7e96afec8370879d0
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2024-10-14 14:20:35 +0200

    asd

---
+++ 2024-2025/gyak241014/JatekosLovese.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak241014
+{
+    internal class JatekosLovese
+    {
+        public List<string> nevek = new List<string>();
+        public List<double> xkordi = new List<double>();
+        public List<double> ykordi = new List<double>();
+        public List<int> sorszam = new List<int>();
+        public JatekosLovese()
+        {
+            StreamReader olvas = new StreamReader("lovesek.txt");
+            
+            while (!olvas.EndOfStream)
+            {
+                int i = 0;
+                i++;
+                string line = olvas.ReadLine();
+                nevek.Add(line.Split(";")[0]);
+                xkordi.Add(Convert.ToDouble(line.Split(";")[1]));
+                ykordi.Add(Convert.ToDouble(line.Split(";")[2]));
+                sorszam.Add(i);
+            }
+        }
+    }
+}
---
+++ 2024-2025/gyak241014/Program.cs
@@ -0,0 +1,4 @@
+﻿using gyak241014;
+
+JatekosLovese valaki = new JatekosLovese();
+Console.WriteLine(valaki.nevek[0]);

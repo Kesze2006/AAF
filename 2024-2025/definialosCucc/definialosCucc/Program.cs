852f2a436cf103610220e719923219d7374db517
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2024-11-14 10:46:26 +0100

    asd

---
+++ 2024-2025/definialosCucc/definialosCucc/Program.cs
@@ -0,0 +1,30 @@
+﻿using operatorredef;
+
+Pont p1 = new Pont(1, 1);
+Pont p2 = new Pont(2, 3);
+Console.WriteLine(p1 + p2);
+Console.WriteLine(p1 + 10);
+Console.WriteLine(10 + p2);
+
+Console.WriteLine(p1 - p2);
+Console.WriteLine(p1 - 10);
+Console.WriteLine(10 - p2);
+
+Console.WriteLine(p1 * p2);
+Console.WriteLine(p1 * 10);
+Console.WriteLine(10 * p2);
+
+Console.WriteLine(p1 / p2);
+Console.WriteLine(p1 / 10);
+Console.WriteLine(10 / p2);
+
+Console.WriteLine(p2++);
+Console.WriteLine(++p2);
+
+Console.WriteLine(p2--);
+Console.WriteLine(--p2);
+
+
+Console.WriteLine(p1.Equals(new Pont(1,1)));
+Console.WriteLine(p1!=p2);
+Console.WriteLine(p1 == new Pont(1,1));

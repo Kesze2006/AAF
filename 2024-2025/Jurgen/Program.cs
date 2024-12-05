08ea8bdc6ab1efd928284fe250838e69e8ce78d4
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2024-12-05 08:41:57 +0100

    asd

---
+++ 2024-2025/Jurgen/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using Jurgen;
+
+penztarca jurgenTalca = new penztarca(10000,15000000);
+int hansi = 4550;
+penztarca faruValaki = new penztarca(10000, 15);
+penztarca lakodalmasCucc = new penztarca(10000, 0);
+int lakodalomPenz = 5000;
+penztarca komuves = new penztarca(150, 0);
+int kezpenzKomuves = 450;
+penztarca adossag = new penztarca(1500, 0);
+int adossagPenz = 1500000000;
+
+Console.Write("Kezdeti tálca: ");
+Console.WriteLine(jurgenTalca);
+
+
+
+Console.WriteLine(jurgenTalca - hansi);
+Console.WriteLine(jurgenTalca - faruValaki);
+Console.WriteLine(jurgenTalca + kezpenzKomuves);
+Console.WriteLine(jurgenTalca + komuves);
+Console.WriteLine(lakodalomPenz + lakodalmasCucc);
+Console.WriteLine(adossagPenz - adossag);
+
+
+
---
+++ 2024-2025/Jurgen/penztarca.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurgen
+{
+    internal class penztarca
+    {
+        public int penz;
+        public int hitel;
+        internal penztarca(int penz, int hitel)
+        {
+            this.penz = penz;
+            this.hitel = hitel;
+        }
+
+        public static penztarca operator -(penztarca talca, int penzKi)
+        {
+            int ujPenz = Convert.ToInt32(talca.penz) - penzKi;
+            return new penztarca(talca.penz = ujPenz,talca.hitel);
+        }
+
+        public static penztarca operator +(penztarca tarca, int penzBe)
+        {
+            int ujPenz = Convert.ToInt32(tarca.penz) + penzBe;
+            return new penztarca(tarca.penz = ujPenz, tarca.hitel);
+        }
+
+        public static penztarca operator -(penztarca elso, penztarca masodik)
+        {
+            return new penztarca(elso.penz,elso.hitel - masodik.hitel); 
+        }
+
+        public static penztarca operator +(int penz, penztarca masodik)
+        {
+            return new penztarca(penz + masodik.penz, masodik.hitel);
+        }
+        public static penztarca operator +(penztarca elso, penztarca masodik)
+        {
+            return new penztarca(elso.penz + masodik.penz, masodik.hitel);
+        }
+        public static penztarca operator -(int penz, penztarca talca)
+        {
+            return new penztarca(penz - talca.penz, talca.hitel);
+        }
+
+
+
+
+        //kezpenz + pénztarca; pénztarca + kezpenz; pénztárca +pénztárca 
+        public override string ToString()
+        {
+            return Convert.ToString(penz)+"euro és a hitel: "+Convert.ToString(hitel);
+        }
+    }
+}

16a21397adf8a57d8cd453bc5856f4c1a397fcc0
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2024-12-16 23:07:04 +0100

    asd

--- 2024-2025/regEpx2/regEpx2/Program.cs
+++ 2024-2025/regEpx2/regEpx2/Program.cs
@@ -4,63 +4,19 @@ using System.Text.RegularExpressions;
 
 string text = File.ReadAllText("generated_log_2024-12-11T11_21_39.997Z.txt");
 
-Regex minta = new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}");
+Regex minta = new Regex(@"""([^""]+)""\s*$""", RegexOptions.Multiline);
+
+//. bármi lehet
+//* bármennyi számú karakter
+//+ az adott karakter után ugyan olyan lesz
+//$ utolsó illeszkedés
+//^első illeszkedés
+//"" ilyen felel meg => "-nek
+//[^valami] kizárja azokat a karaktereket, mármint a szövegben nem lehetnek pl:""[^""]""-nem lehet " a "" között
 
 var talalat = minta.Matches(text);
 Console.WriteLine(talalat.Count);
 foreach (Match elem in talalat)
 {
     Console.WriteLine(elem);
-}
-
-Dictionary<Match, int> stats = new Dictionary<Match, int>();
-foreach (Match elem in talalat)
-{
-    if (stats.ContainsKey(elem))
-    {
-        stats[elem]++;
-    }
-    else
-    {
-        stats.Add(elem, 1);
-    }
-}
-foreach (KeyValuePair<Match, int> elem in stats)
-{
-    if (elem.Value != 1)
-    {
-        Console.WriteLine(elem);
-    }
-}
-
-Regex minta2 = new Regex(@"GET (/\S*)");
-
-var talalat2 = minta2.Matches(text);
-Console.WriteLine(talalat2.Count);
-foreach (Match elem in talalat2)
-{
-    Console.WriteLine(elem.Groups[1].Value);
-}
-
-Dictionary<Match, int> stats2 = new Dictionary<Match, int>();
-foreach (Match elem in talalat2)
-{
-    if (stats2.ContainsKey(elem))
-    {
-        stats[elem]++;
-    }
-    else
-    {
-        stats2.Add(elem, 1);
-    }
-}
-
-
-Regex minta3 = new Regex(@"GET (/\S*)");
-
-var talalat3 = minta3.Matches(text);
-Console.WriteLine(talalat3.Count);
-foreach (Match elem in talalat3)
-{
-    Console.WriteLine(elem.Groups[1].Value);
 }

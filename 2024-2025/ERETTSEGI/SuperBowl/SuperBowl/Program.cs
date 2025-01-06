34c57519edf995b843800053927cadae1fa980d7
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2025-01-06 14:20:23 +0100

    asd

---
+++ 2024-2025/ERETTSEGI/SuperBowl/SuperBowl/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using SuperBowl;
+
+StreamReader olvas = new StreamReader("SuperBowl.txt");
+
+olvas.ReadLine();
+int sorszam = 0;
+while (!olvas.EndOfStream && sorszam <= 100)
+{
+    sorszam++;
+    string line = olvas.ReadLine();
+    string[] adatok = line.Split(";");
+}
---
+++ 2024-2025/ERETTSEGI/SuperBowl/SuperBowl/adatok.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBowl
+{
+    internal class adatok
+    {
+        public adatok()
+        {
+            string romaiSzam;
+            string datum;
+            string gyoztes;
+            string eredmeny;
+            string vesztes;
+            string helyszin;
+            string varosAllam;
+            int nezoSzam;
+        }
+    }
+}

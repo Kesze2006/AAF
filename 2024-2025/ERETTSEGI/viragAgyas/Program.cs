7cb7210c23a4b086e97059be33ae663f1eae46d3
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2025-01-30 13:34:12 +0100

    asd

---
+++ 2024-2025/ERETTSEGI/viragAgyas/Program.cs
@@ -0,0 +1,10 @@
+﻿using viragAgyas;
+
+List<adatok> viragok =  new List<adatok>();
+
+string[] sorok = File.ReadAllLines("felajanlas.txt");
+int agyasDB = int.Parse(sorok[0]);
+for (int i = 1; i < sorok.Length; i++)
+{
+    viragok.Add(new adatok(sorok[i]));
+}
---
+++ 2024-2025/ERETTSEGI/viragAgyas/adatok.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viragAgyas
+{
+    internal class adatok
+    {
+        public int kezd;
+        public int veg;
+        public string szin;
+
+        public adatok(string sor)
+        {
+            string[] adatok = sor.Split(' ');
+            kezd = int.Parse(adatok[0]);
+            veg = int.Parse(adatok[1]);
+            szin = adatok[2];
+        }
+    }
+}

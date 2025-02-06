da8769e01fc8f5bd562879b0e119fd23ea62930d
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2025-02-06 10:34:39 +0100

    asd

--- 2024-2025/ERETTSEGI/viragAgyas/Program.cs
+++ 2024-2025/ERETTSEGI/viragAgyas/Program.cs
@@ -21,14 +21,31 @@ Console.WriteLine("3. feladat: A bejárat mindkét oldalán ültetők: ");
 Console.WriteLine(String.Join(", ", viragok.Where(e => e.kapuBentVan).Select(e => e.sorSzam)));
 
 //4.feladat
-Console.WriteLine("4.feladat\nAdja meg a ");
+Console.Write("4.feladat\nAdja meg a ");
 int be = int.Parse(Console.ReadLine());
 int darabok = 0;
+string szin = "";
+HashSet<string> szinek = new HashSet<string>();
 for (int i = 0; i < viragok.Count; i++)
 {
     if (viragok[i].benneVanEz(be))
     {
        darabok++;
+        if (szin == "")
+        {
+            szin = viragok[i].szin;
+        }
+        szinek.Add(viragok[i].szin);
     }
 }
 Console.WriteLine("A felajánlások száma: {0}", darabok);
+
+if (darabok == 0)
+{
+    Console.WriteLine("Ezt az ágyást nem ültetik be");
+}
+else
+{
+    Console.WriteLine("Virágágyás színe ha csak az első ültet: {0}",szin);
+    Console.WriteLine("A virágágyás színei: {0}",String.Join(" ",szinek));
+}
--- 2024-2025/ERETTSEGI/viragAgyas/obj/Debug/net8.0/viragAgyas.AssemblyInfo.cs
+++ 2024-2025/ERETTSEGI/viragAgyas/obj/Debug/net8.0/viragAgyas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("viragAgyas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62b04462b53ed0ef22bb3ce44ace87c844f5260c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b3e23e1d96eeb4ab70418935629e34720c26672")]
 [assembly: System.Reflection.AssemblyProductAttribute("viragAgyas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("viragAgyas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

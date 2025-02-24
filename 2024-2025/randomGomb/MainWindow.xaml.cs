6cadece148dd3b6ebf13627c0cb77cd0c9ca798c
Keszericze Ákos <keszericzea21@ady-nagyatad.sulinet.hu>
2025-02-24 14:02:46 +0100

    asd

---
+++ 2024-2025/randomGomb/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace randomGomb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void randomGomb_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random szam = new Random();
+            double x = szam.NextDouble();
+            double y = szam.NextDouble();
+            randomGomb.Margin =new Thickness(x,y,0,0);
+        }
+    }
+}

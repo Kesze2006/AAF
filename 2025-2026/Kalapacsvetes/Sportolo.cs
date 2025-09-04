444b1d8b15a02da3742033435a6e9d48703deb53
unknown <keszericze.akos.21@ady-nagyatad.lan>
2025-09-04 08:40:53 +0200

    Amiben sehol nem tartunk

---
+++ 2025-2026/Kalapacsvetes/Sportolo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalapacsvetes
+{
+    internal class Sportolo
+    {
+        public int helyezes;
+        public double eredmeny;
+        public string nev;
+        public string orszagKod;
+        public string helyszin;
+        public DateOnly datum;
+
+        public Sportolo(int helyezes,double eredmeny,string nev, string orszagKod, string helyszin, DateOnly datum)
+        {
+            this.helyezes = helyezes;
+            this.nev = nev;
+            this.eredmeny = eredmeny;
+            this.orszagKod = orszagKod;
+            this.helyszin = helyszin;
+            this.datum = datum;
+        }
+    }
+}

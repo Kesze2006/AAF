a99d03cc41fdbab613a77d57d389e234a199469d
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2025-09-09 13:34:04 +0200

    asd

--- 2025-2026/Fuggohidak/Fuggohid.cs
+++ 2025-2026/Fuggohidak/Fuggohid.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,29 @@ namespace Fuggohidak
 {
     internal class Fuggohid
     {
-
-        public Fuggohid()
+        public int helyezes;
+        public string hidNeve;
+        public string foldrajziHely;
+        public string orszag;
+        public int hossza;
+        public int atadasEve;
+        public Fuggohid(string[] sor)
         {
             
+        }
+
+        public static List<Fuggohid> Olvas()
+        {
+            List<Fuggohid> list = new List<Fuggohid>();
+            StreamReader olvaso = new StreamReader("fuggohidak.csv");
+            olvaso.ReadLine();
+            while (!olvaso.EndOfStream)
+            {
+                string line = olvaso.ReadLine();
+                string[] sor = line.Split("\t");
+                list.Add(new Fuggohid(sor));
+            }
+            return list;
         }
     }
 }

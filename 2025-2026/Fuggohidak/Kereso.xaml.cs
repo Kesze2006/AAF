5572bacfaa4e9a8f9eaa4cff41c5f73ccfada347
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2025-09-14 12:30:35 +0200

    asd

---
+++ 2025-2026/Fuggohidak/Kereso.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Fuggohidak
+{
+    /// <summary>
+    /// Interaction logic for Kereso.xaml
+    /// </summary>
+    public partial class Kereso : Window
+    {
+        public Kereso()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow foAblak = new MainWindow();
+            this.Close();
+            foAblak.Show();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> orszagok = new List<string>();
+            foreach (Fuggohid hid in Fuggohid.Olvas())
+            {
+                if (!orszagok.Contains(hid.orszag))
+                {
+                    orszagok.Add(hid.orszag);
+                }
+            }
+            comboBox.ItemsSource = orszagok;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void comboBox_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            hidak.Text = "";
+            foreach (Fuggohid hid in Fuggohid.Olvas())
+            {
+                if (comboBox.Text == hid.orszag)
+                {
+                    if (hossz.IsChecked == true)
+                    {
+                        if (hid.hossza > 1000)
+                        {
+                            hidak.Text += hid.hidNeve + "\n";
+                        }
+                    }
+                    else
+                    {
+                        hidak.Text += hid.hidNeve + "\n";
+                    }
+                }
+            }
+        }
+    }
+}
--- 2025-2026/Fuggohidak/MainWindow.xaml.cs
+++ 2025-2026/Fuggohidak/MainWindow.xaml.cs
@@ -28,5 +28,62 @@ namespace Fuggohidak
                 listaBox.Items.Add(hid.hidNeve);
             }
         }
+
+        private void listaBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            foreach (Fuggohid hid in hidak)
+            {
+                if (listaBox.SelectedIndex == hid.helyezes-1)
+                {
+                    hely.Text = hid.foldrajziHely;
+                    hossz.Text = hid.hossza.ToString();
+                    orszag.Text = hid.orszag;
+                    ev.Text = hid.atadasEve.ToString();
+                }
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            int db = 0;
+            foreach (Fuggohid hid in hidak)
+            {
+                if (hid.atadasEve < 2000)
+                {
+                    db++;
+                }
+            }
+            darabSzam.Text = db.ToString();
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            int db = 0;
+            foreach (Fuggohid hid in hidak)
+            {
+                if (hid.atadasEve >= 2000)
+                {
+                    db++;
+                }
+            }
+            darabSzam.Text = db.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Kereso ujablak = new Kereso();
+            this.Hide();
+            ujablak.Show();
+        }
     }
 }

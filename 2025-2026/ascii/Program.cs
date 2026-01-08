bf4a47d4b42a81b279d4ca82335d36a71ee79bff
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2026-01-08 12:24:29 +0100

    asd

---
+++ 2025-2026/ascii/Program.cs
@@ -0,0 +1,42 @@
+﻿//1.feladat
+Console.WriteLine("1.feladat");
+string[] konyv = File.ReadAllLines("konyv.txt");
+foreach (string sor in konyv)
+{
+    Console.WriteLine(sor);
+}
+
+//2.feladat
+Console.WriteLine("2.feladat");
+Console.Write("Kérem adja meg az ismétlések számát: ");
+int ismSzám = int.Parse(Console.ReadLine());
+string sokKonyv = "";
+char z = ' ';
+int t = 4;
+int l = 0;
+for (int j = 0; j < konyv.Length; j++)
+{
+    if (j == 0)
+    {
+        for (int i = 0; i < ismSzám; i++)
+        {
+            sokKonyv += new string(z, t) + konyv[j].Trim() + "  " + "|";
+        }
+        sokKonyv += "\n";
+        t--;
+        l++;
+    }
+    else
+    {
+        for (int i = 0; i < ismSzám; i++)
+        {
+            sokKonyv += new string(z, t) + konyv[j].Trim() + new string(z, l) + "|";
+        }
+    sokKonyv += "\n";
+    t--;
+    l++;
+    }
+}
+Console.WriteLine(sokKonyv);
+
+//3.feladat
